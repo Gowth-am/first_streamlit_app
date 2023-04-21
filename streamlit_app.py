331189a7b2867de301e141239b4a18871852b0fd
Gowth-am <96461242+Gowth-am@users.noreply.github.com>
2023-04-21 08:56:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
-streamlit.title('Hey there !!!')
-streamlit.header('Heading section')
-streamlit.text('this is a text section')
+streamlit.header('🥗 Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
